a5d9769f087e486d7625144d60133f188da6a0e8
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2017-12-18 13:50:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".mscrol").addClass("hidden").viewportChecker({
+        classToAdd: "visible fadeIn animated",
+        offset: 200
+    });
+});
+
+$(document).ready(function() {
+    $(".scrol").addClass("hidden").viewportChecker({
+        classToAdd: "visible fadeIn animated",
+        offset: 50
+    });
+});
