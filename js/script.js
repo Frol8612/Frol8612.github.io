3a773c1e95c7a73d2b3e7448057c9016a053d067
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2017-12-24 21:41:12 +0300

    Commit to master

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".mscrol").addClass("hidden").viewportChecker({
         classToAdd: "visible fadeIn animated",
         offset: 200
     });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".scrol").addClass("hidden").viewportChecker({
         classToAdd: "visible fadeIn animated",
         offset: 15
     });
 });
 
-$('.drop').click(function() {
+$('.drop').click(function () {
     $('.menu').fadeToggle();
     $('.drop').toggleClass('gum');
 });
