a6d404089d1560337676dcc46a5300524f07bc1b
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2017-12-25 19:25:00 +0300

    Commit to master

--- js/script.js
+++ js/script.js
@@ -12,16 +12,29 @@ $(document).ready(function () {
     });
 });
 
-$('.drop').click(function () {
-    $('.menu').fadeToggle();
-    $('.drop').toggleClass('gum');
+$(function () {
+    $('.drop').click(function () {
+        $('.drop').toggleClass('gum');
+        if ($('#menu').css('display') == 'none') {
+            $('#menu').fadeIn();
+        } else {
+            $('#menu').fadeOut();
+        }
+    });
+    $(window).resize(function () {
+        if ($(window).width() >= 875) {
+            $('#menu').css('display','block');
+        } else {
+            $('#menu').css('display','none');
+        }
+    });
 });
 
 $(function() {
-    $('.menu').hover(function (e) {
+    $('.menu').hover(function () {
         $('#head').toggleClass('active'); 
         $('#mn1').toggleClass('active1');
         $('.mn23').toggleClass('active23');
         $('#mn4').toggleClass('active4');
-      })
+    });
 });
