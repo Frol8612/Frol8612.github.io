951bad9e3d338d541b3cd08f625076af7bb35710
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-02-12 09:30:40 +0300

    Commit ti master

---
+++ script.js
@@ -0,0 +1,49 @@
+
+let t1 = document.getElementById('t1'),
+    num = Math.floor(Math.random() * 11),
+    sp = document.getElementById('sp');
+    sp.innerHTML = 3;
+    console.log(num);
+
+function addNum (e) {
+    if(e.key == 'Enter') {
+        if (!isNaN(parseFloat(t1.value))) {
+            if (parseInt(t1.value) || t1.value == 0) {
+                if (t1.value < 0 || t1.value > 10) {
+                    alert('Введите значение от 1 до 10');
+                } else {
+                    if (t1.value > num) {
+                        alert('Число больше данного');
+                        
+                    } else if (t1.value < num) {
+                        alert('Число меньше данного');
+                    } else {
+                        alert('Вы угадали!!!');
+                        location.reload();
+                        alert('Сгенерированно новое число!!!');
+                        return;
+                    }
+                }
+            } else {
+                alert('Введите целое число!');
+            }
+        } else {
+            alert('Ведите число!');
+        }
+        t1.value = null;
+        numPop();
+    }
+}
+
+function numPop() { 
+    if (sp.innerHTML == 1) {
+        alert('Попытки использованны!!!');
+        location.reload();
+        alert('Сгенерированно новое число!!!');
+    } else {
+        sp.innerHTML--;
+        console.log(sp.innerHTML);
+    }
+}
+
+t1.addEventListener('keydown', addNum);
